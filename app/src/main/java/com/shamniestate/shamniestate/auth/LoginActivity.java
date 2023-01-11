1b56af7dd45236097d555bfc3808511aaa86da2d
Bhawesh <bhaweshjain384@gmail.com>
2023-01-11 23:38:15 +0530

    Fixed Theme

---
+++ app/src/main/java/com/shamniestate/shamniestate/auth/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.shamniestate.shamniestate.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.shamniestate.shamniestate.R;
+import com.shamniestate.shamniestate.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    private Activity activity;
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        activity = LoginActivity.this;
+
+
+    }
+}
